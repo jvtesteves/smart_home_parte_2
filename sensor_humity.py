a2f294e7f21652ddbc30d33d495b4cf052040c62
Joao Esteves <joaovtesteves2002@gmail.com>
2023-11-13 14:08:42 -0300

    novas filas para sensores

--- sensor_humity.py
+++ sensor_humity.py
@@ -4,24 +4,31 @@ import time
 from random import uniform
 
 # Configurações do RabbitMQ
-rabbitmq_host = 'localhost'  # ou o IP/host onde seu RabbitMQ está rodando
+rabbitmq_host = 'localhost'
 queue_name = 'sensor_data'
+humidity_queue_name = 'sensor_data_humidity'  # Nova fila para umidade
 
 # Estabelece conexão com o RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
 channel = connection.channel()
 
-# Declara a fila onde as mensagens serão publicadas
+# Declara as filas onde as mensagens serão publicadas
 channel.queue_declare(queue=queue_name)
+channel.queue_declare(queue=humidity_queue_name)  # Declaração da nova fila
+
 
 def publish_sensor_data():
     while True:
-        # Simula a leitura do sensor de temperatura
+        # Simula a leitura do sensor de umidade
         humidity = round(uniform(20.0, 90.0), 0)
         message = json.dumps({'sensor_type': 'humidity', 'value': humidity})
-        # Publica a mensagem na fila
+
+        # Publica a mensagem nas filas
         channel.basic_publish(exchange='',
                               routing_key=queue_name,
+                              body=message)
+        channel.basic_publish(exchange='',
+                              routing_key=humidity_queue_name,
                               body=message)
         print(f"[x] Sent {message}")
         time.sleep(10)  # Intervalo entre as publicações
--- sensor_temp.py
+++ sensor_temp.py
@@ -4,15 +4,17 @@ import time
 from random import uniform
 
 # Configurações do RabbitMQ
-rabbitmq_host = 'localhost'  # ou o IP/host onde seu RabbitMQ está rodando
+rabbitmq_host = 'localhost'
 queue_name = 'sensor_data'
+temperature_queue_name = 'sensor_data_temperature'  # Nova fila para temperatura
 
 # Estabelece conexão com o RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
 channel = connection.channel()
 
-# Declara a fila onde as mensagens serão publicadas
+# Declara as filas onde as mensagens serão publicadas
 channel.queue_declare(queue=queue_name)
+channel.queue_declare(queue=temperature_queue_name)  # Declaração da nova fila
 
 
 def publish_sensor_data():
@@ -21,9 +23,12 @@ def publish_sensor_data():
         temperature = round(uniform(18.0, 26.0), 0)
         message = json.dumps({'sensor_type': 'temperature', 'value': temperature})
 
-        # Publica a mensagem na fila
+        # Publica a mensagem nas filas
         channel.basic_publish(exchange='',
                               routing_key=queue_name,
+                              body=message)
+        channel.basic_publish(exchange='',
+                              routing_key=temperature_queue_name,
                               body=message)
         print(f"[x] Sent {message}")
         time.sleep(20)  # Intervalo entre as publicações
